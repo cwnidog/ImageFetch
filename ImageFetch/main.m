ee4993f60f491a84798c3a83c44b99c5a9cc9835
cwnidog <cwnidog@comcast.net>
2015-07-28 16:02:50 -0700

    Initial files

---
+++ ImageFetch/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ImageFetch
+//
+//  Created by John Leonard on 7/28/15.
+//  Copyright (c) 2015 John Leonard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
