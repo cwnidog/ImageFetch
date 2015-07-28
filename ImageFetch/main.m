92ba5df3cb8a3013b2c93a8a74471df512aa642f
cwnidog <cwnidog@comcast.net>
2015-07-28 16:21:29 -0700

    Adding code to go out and get an image as NSData and store it in a file

--- ImageFetch/main.m
+++ ImageFetch/main.m
@@ -8,10 +8,33 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
-  }
+int main(int argc, const char *argv[])
+{
+  @autoreleasepool
+  {
+    NSURL *url = [NSURL URLWithString:@"http://www.gear-wiki.com/showfile/2251/gearwikilogo.png/269"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSError *error = nil;
+    
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:NULL error:&error];
+    
+    if (!data)
+    {
+      NSLog(@"fetch failed: %@", [error localizedDescription]);
+      return 1;
+    }
+    
+    NSLog(@"The file is %lu bytes", (unsigned long)[data length]);
+    BOOL written = [data writeToFile:@"/tmp/google.png" options:0 error:&error];
+    
+    if (!written)
+    {
+      NSLog(@"write failed: %@", [error localizedDescription]);
+      return 1;
+    }
+    
+    NSLog(@"Success");
+    
+  } // @autoreleasepool
     return 0;
-}
+} // main()
